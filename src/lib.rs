60e9c5b0faffc18edf45009083a8b5bad2907ecc
pierre <pierreguthauser@gmail.com>
2017-03-09 23:20:54 +0100

    Puted all in one file

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,22 @@ extern crate rustc;
 extern crate rustc_plugin;
 extern crate syntax;
 
-mod macros;
-
 use rustc_plugin::Registry;
+use syntax::parse::token;
+use syntax::tokenstream::TokenTree;
+use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacEager};
+use syntax::ext::build::AstBuilder;
+use syntax::ext::quote::rt::Span;
+
+#[rustfmt_skip]
+pub fn rocket_swagger(ecx: &mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'static> {
+    let mut parser = ecx.new_parser_from_tts(args);
+
+    println!("{:?}", parser.parse_item());
+    DummyResult::any(sp)
+}
+
 
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_macro("rocket_swagger", macros::rocket_swagger);
+    reg.register_macro("rocket_swagger", rocket_swagger);
 }
