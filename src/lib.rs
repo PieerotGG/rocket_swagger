e299a3efd3d0d971aa813b26eea7f96549caf46d
pierre <pierreguthauser@gmail.com>
2017-03-09 23:23:23 +0100

    Try something

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacEager};
 use syntax::ext::build::AstBuilder;
 use syntax::ext::quote::rt::Span;
 
-#[rustfmt_skip]
 pub fn rocket_swagger(ecx: &mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'static> {
     let mut parser = ecx.new_parser_from_tts(args);
 
